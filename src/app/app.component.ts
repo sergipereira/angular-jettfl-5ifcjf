4acf3b6d150add27386ee9a565882434f61e99b5
sergipereira <info@off-brand.pt>
2020-05-11 22:59:25 +0100

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,23 @@ import { Component } from '@angular/core';
   selector: 'nz-demo-card-border-less',
   template: `
     <div style="background: #ECECEC;padding:30px;">
-      <nz-card style="width:300px;" [nzBordered]="false" nzTitle="Card title" [nzExtra]="extraTemplate">
+     <nz-card style="width:500px;" [nzBordered]="false" nzTitle="Card title" [nzExtra]="extraTemplate">
+     
         <p>Card content</p>
         <p>Card content</p>
         <p>Card content</p>
       </nz-card>
+    <p>© 2020 · WEMOVIT. Todos os direitos reservados. <a href="#">Termos de Uso</a> | <a href="#">Garantia de Segurança</a>
     </div>
     <ng-template #extraTemplate>
       <a>More</a>
     </ng-template>
+
   `,
   styles: [
     `
       p {
-        margin: 0;
+        margin: 1;
       }
     `
   ]
